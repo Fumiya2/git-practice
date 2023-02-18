8c0ac3e8d3a302a94c47aa95242e1fd7ce84a858
Fumiya2 <124821309+Fumiya2@users.noreply.github.com>
2023-02-18 05:16:17 +0000

    [Update]7-5

---
+++ lesson5.rb
@@ -0,0 +1,13 @@
+total_price = "100"
+
+if total_price > 100
+    puts "みかんを購入。所持金に余りあり。"
+end
+
+elsif total_price == 100
+    puts "みかんを購入。所持金は0円。"
+end
+
+else total_price < 100
+    puts "みかんを購入することが出来ません"
+end
